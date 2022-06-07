7880d0851137fa93aa16b43d80c9d6f4fcd78a05
winston kiprop <winstonekip50@gmail.com>
2022-06-07 23:13:23 +0300

    commit tree 791380030a1a910b331b1ae27dcb9cb5e3d66578

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+
+ function saturdayFun(activity ='roller-skate' ){
+     return `This Saturday, I want to ${activity}!`
+ }
+  function  mondayWork(name ='go to the office') {
+      return`This Monday, I will ${name}.`
+  }
+  function wrapAdjective(style) {
+    let result =(function(activity) {
+        return "You are " +style+ activity+
+        style + "!"
+    })
+    let emphatic = result;
+    return emphatic;   
+} 
